d4c3c275ae56c31dee02e04e99d9e699ee2b47fe
Behzad <saeedibehzad@gmail.com>
2021-11-12 16:50:13 +0330

    use useLayoutEffect in order to be know when screen size changed

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,12 +1,26 @@
-import { Children } from "react";
+import { children ,useLayoutEffect,useState} from "react";
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 const Layout = ({children}) => {
+    const [size, setSize] = useState(0);
+    
+        useLayoutEffect(() => {
+            function updateSize() {
+            setSize(window.innerWidth);
+          }
+          window.addEventListener('resize', updateSize);
+          updateSize();
+          return () => window.removeEventListener('resize', updateSize);
+        }, []);
+        
+    
+    console.log(size)
+
     return (
         <>
             <Header/>
                 {children}
-            <Footer/>
+             <Footer/>
         </>
      );
 }
