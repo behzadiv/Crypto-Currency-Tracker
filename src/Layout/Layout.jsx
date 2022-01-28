78a5b24fcfc106f8a0f34f4fb6385912a1308859
Behzad <saeedibehzad@gmail.com>
2022-01-28 13:45:53 +0330

    remove useLayoutEffect

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,21 +1,8 @@
-import { children ,useLayoutEffect,useState} from "react";
+import { children ,useEffect,useState} from "react";
 import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 const Layout = ({children}) => {
-    const [size, setSize] = useState(0);
-    
-        useLayoutEffect(() => {
-            function updateSize() {
-            setSize(window.innerWidth);
-          }
-          window.addEventListener('resize', updateSize);
-          updateSize();
-          return () => window.removeEventListener('resize', updateSize);
-        }, []);
-        
-    
-    console.log(size)
-
+  
     return (
         <>
             <Header/>
