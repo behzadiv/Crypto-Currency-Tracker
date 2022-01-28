db725732a1c84c09e1d8ad8442d92cf9382ed23f
Behzad <saeedibehzad@gmail.com>
2022-01-28 13:44:22 +0330

    stop set interval when user search crypto

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,7 +10,6 @@ const HomePage = () => {
   const [favoriteList, setFavoriteList] = useState(
     JSON.parse(localStorage.getItem("data")) || []
   );
-
   const userInput = useSearchInput();
   const addToFavoriteList = (item) => {
     console.log(item);
@@ -29,7 +28,7 @@ const HomePage = () => {
         `https://api.nomics.com/v1/currencies/ticker?key=2894a1f621619bd9c9778bcb9b476fcc211f623d&ids=&interval=1h,1d,7d,30d,365d&convert=USD&_ga=2.244143877.18783313.1633931534-1081431702.1633931534&per-page=200`
       )
       .then((response) => {
-        console.log("ok", newList);
+        //console.log("ok", newList);
         setAllData(response.data);
         setFilteredData(response.data);
       })
@@ -44,9 +43,12 @@ const HomePage = () => {
 
   useEffect(() => {
     myRequest();
-    setInterval(myRequest, 5000);
+    if(userInput.length===0) setInterval(()=>myRequest, 5000);
     localStorage.setItem("data", JSON.stringify(favoriteList));
-    console.log("errrrr");
+    //console.log(userInput.length);
+    
+      // if(userInput.length>0)clearInterval(myTimer)
+   
   }, [favoriteList]);
 
   const searchInput = (x) => {
@@ -54,6 +56,7 @@ const HomePage = () => {
       return item.name.toLowerCase().includes(x.toLowerCase());
     });
     setFilteredData(filtered);
+    //console.log(filtered);
   };
   return (
     <div>
